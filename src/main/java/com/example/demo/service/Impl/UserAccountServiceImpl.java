7f3507d2d5818c28d8ecdb3963903f9ce9206c47
bala-amypo <bala@amypo.in>
2025-12-22 07:59:32 +0000

    User commit

--- src/main/java/com/example/demo/service/Impl/UserAccountServiceImpl.java
+++ src/main/java/com/example/demo/service/Impl/UserAccountServiceImpl.java
@@ -5,7 +5,7 @@ import com.example.demo.exception.BadRequestException;
 import com.example.demo.exception.ResourceNotFoundException;
 import com.example.demo.repository.UserAccountRepository;
 import com.example.demo.service.UserAccountService;
-import org.springframework.security.crypto.password.PasswordEncoder;
+
 
 public class UserAccountServiceImpl implements UserAccountService {
 
