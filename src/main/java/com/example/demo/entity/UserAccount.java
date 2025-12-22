65fbcb953fb0b7f8d71344e295202b034a320eae
bala-amypo <bala@amypo.in>
2025-12-22 03:57:02 +0000

    User commit

--- src/main/java/com/example/demo/entity/UserAccount.java
+++ src/main/java/com/example/demo/entity/UserAccount.java
@@ -1,38 +1,20 @@
 package com.example.demo.entity;
 
 import jakarta.persistence.*;
-import java.time.LocalDateTime;
+import lombok.*;
 
 @Entity
-@Table(name = "user_accounts")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserAccount {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String fullName;
-
-    @Column(unique = true)
     private String email;
-
     private String password;
     private String role;
-    private LocalDateTime createdAt;
-
-    public UserAccount() {}
-
-    public UserAccount(Long id, String fullName, String email, String password, String role) {
-        this.id = id;
-        this.fullName = fullName;
-        this.email = email;
-        this.password = password;
-        this.role = role;
-    }
-
-    @PrePersist
-    void prePersist() {
-        if (role == null) role = "USER";
-        createdAt = LocalDateTime.now();
-    }
 }
